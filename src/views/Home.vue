51f3065102dc88d5db48bcd1f5f62dc6bd20f2c3
Amine <fajry599@gmail.com>
2021-03-22 14:18:45 +0100

    Add viewtify

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <v-app-bar
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+
+    </v-row>
+  </v-app-bar>
+
+
+
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+        'Panier',
+      ],
+    }),
+  }
+</script>
