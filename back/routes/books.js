166d436d0eeef605d1df5f3a6f6a563528ae08b4
Amine <fajry599@gmail.com>
2021-04-07 20:52:08 +0200

    "ajout de update , get, delete"

--- back/routes/books.js
+++ back/routes/books.js
@@ -19,8 +19,6 @@ function query(...args) {
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-    
-
     query('SELECT * FROM books', function (error, results, fields) {
     
     if (error) throw error;
@@ -29,6 +27,21 @@ router.get('/', function(req, res, next) {
     });
 
    
+});
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+
+  var id = req.params.id;
+
+  query('SELECT * FROM books WHERE id = ?',[id], function (error, results, fields) {
+  
+  if (error) throw error;
+  res.send(results);
+  console.log('The solution is: ', results[0].solution);
+  });
+
+ 
 });
 
 
@@ -40,7 +53,7 @@ router.post('/',function(req,res,next)
 
   console.log(image);
 
-  query('INSERT INTO books (name,image,description) VALUES (?,?,?) ', [name, image, description], function (error, results, fields) {
+    query('INSERT INTO books (name,image,description) VALUES (?,?,?) ', [name, image, description], function (error, results, fields) {
     
     if (error) throw error;
     res.send(results);
@@ -50,9 +63,41 @@ router.post('/',function(req,res,next)
 
 });
 
+router.patch('/:id',(req,res) =>
+{
+
+  var id = req.params.id;
+  var name = req.body.todoInput ;
+  var description = req.body.txtdescription;
+  var image = req.body.urlimage;
+
+  query('UPDATE books SET name = ? , image = ? , description = ?  WHERE  id = ?', [name, image, description,id], function (error, results, fields) {
+    
+    if (error) throw error;
+    res.send(results);
+    console.log('The solution is: ', results);
+
+
+  });
 
 
 
+})
+
+router.delete('/:id',(req,res) =>
+{
+
+  var id = req.params.id;
+  var name = req.body.todoInput ;
+  var description = req.body.txtdescription;
+  var image = req.body.urlimage;
+
+  query('DELETE FROM books WHERE id = ? ', [id], function (error, results, fields) {
+    if (error) throw error;
+    console.log('deleted ' + results.affectedRows + ' rows');
+  });
+
+})
 
 
 module.exports = router;
